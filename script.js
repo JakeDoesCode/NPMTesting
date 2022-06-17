cac1bdc9907bdd3e969bdfa272a0b989836b4219
Jake <classicjakeeod@gmail.com>
2022-06-17 11:21:23 -0500

    completed TOP assignment

--- script.js
+++ script.js
@@ -25,4 +25,42 @@ calculator.divide = function (a, b) {
   return a / b;
 };
 
-export { firstCap, reverseString, calculator };
+
+
+const caesar = function(string, shift) {
+  return string
+    .split("")
+    .map(char => shiftChar(char, shift))
+    .join("");
+};
+
+const codeSet = code => (code < 97 ? 65 : 97);
+
+const mod = (n, m) => (n % m + m) % m;
+
+const shiftChar = (char, shift) => {
+  const code = char.charCodeAt();
+
+  if ((code >= 65 && code <= 90) || (code >= 97 && code <= 122)) {
+    return String.fromCharCode(
+      mod(code + shift - codeSet(code), 26) + codeSet(code)
+    );
+  }
+  return char;
+};
+
+
+function analyze(...arr){
+  let arrObj= { }
+  let newArr = arr[0].sort((a,b) => a-b)
+  let arrayLength = arr[0].length 
+  arrObj.length = arrayLength
+  arrObj.min = newArr[0]
+  arrObj.max = newArr[newArr.length - 1]
+  const sum = newArr.reduce((a, b) => a + b, 0);
+  arrObj.average = sum/arrayLength
+  return arrObj
+}
+
+
+export { firstCap, reverseString, calculator, caesar, analyze}
--- script.test.js
+++ script.test.js
@@ -1,5 +1,5 @@
-import { firstCap, reverseString, calculator } from './script';
-
+import { firstCap, reverseString, calculator, caesar, analyze } from './script';
+//return first capitalized letter of string
 test('Does not return first capital letter.', () => {
   expect(firstCap('donKey')).toBe('K');
 });
@@ -12,6 +12,7 @@ test('Does not return first capital letter.', () => {
   expect(firstCap('arrrRgggHHH')).toBe('R');
 });
 
+//reverse string
 test('Does not return reversed string.', () => {
   expect(reverseString('ronald')).toBe('dlanor');
 });
@@ -24,6 +25,7 @@ test('Does not return reversed string.', () => {
   expect(reverseString('rollo')).toBe('ollor');
 });
 
+//calculator
 test('Incorrect value', () => {
   expect(calculator.add(3, 7)).toBe(10);
 });
@@ -38,4 +40,36 @@ test('Incorrect value', () => {
 
 test('Incorrect value', () => {
   expect(calculator.divide(25, 5)).toBe(5);
+});
+
+//ceasarCipher shift letters
+test('Incorrect return', () => {
+  expect(caesar('RUN',1)).toBe('SVO');
+});
+
+
+test('Incorrect return', () => {
+  expect(caesar('ABcD!',13)).toBe('NOpQ!');
+});
+
+test('Incorrect return', () => {
+  expect(caesar('aBCD',13)).toBe('nOPQ');
+});
+//analyze array
+test("Test 3", () => {
+  expect(analyze([5, 1, 2, 3, 7, 1])).toEqual({
+    average: 19 / 6,
+    min: 1,
+    max: 7,
+    length: 6,
+  });
+});
+
+test("Test 2", () => {
+  expect(analyze([1, 1, 1, 1, 1])).toEqual({
+    average: 1,
+    min: 1,
+    max: 1,
+    length: 5,
+  });
 });
